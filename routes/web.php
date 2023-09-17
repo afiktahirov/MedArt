00a5af397f1180d3380ed78a13e3205b2463c1ec
Afik Tahirov <afik.t38@gmail.com>
2023-09-17 17:43:30 +0400

    updated

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,6 @@ Route::prefix('admin')->group(function () {
         Route::get("/slider/deactive",[AdminController::class ,"slider_d"])->name("admin.dslider");
         Route::post("/slider/active",[AdminController::class ,"slider_d_active"])->name("admin.dsilder.active");
         Route::post("/slider/deactive",[AdminController::class ,"slider_d_deactive"])->name("admin.dsilder.deactive");
-
         Route::get("/test/tiny",[AdminController::class,"test_tiny"])->name("admin.test_tiny");
 
     });
