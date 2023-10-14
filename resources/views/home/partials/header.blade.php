3ec85ae65ddca98b71d1a9a3f66a829d10c1346c
Afik Tahirov <afik.t38@gmail.com>
2023-10-14 17:32:12 +0400

    update

--- resources/views/home/partials/header.blade.php
+++ resources/views/home/partials/header.blade.php
@@ -2,16 +2,16 @@
     <div class="swiper-wrapper">
         @foreach ($sliders as $slider)
             @php
-                if (!count($slider->languages)){
-                    $text = "";
-                    $title = "";
-                }else{
+                if (!count($slider->languages)) {
+                    $text = '';
+                    $title = '';
+                } else {
                     $text = $slider->languages[0]->text;
                     $title = $slider->languages[0]->title;
                 }
             @endphp
             <div class="swiper-slide hero-content-wrap"
-                style="background-image: url('{{ asset("/sliders/$slider->image") }}')">
+                style="background-image: url('{{ asset("storage/uploads/sliders/$slider->image") }}')">
                 <div class="hero-content-overlay position-absolute w-100 h-100">
                     <div class="container h-100">
                         <div class="row h-100">
@@ -25,8 +25,8 @@
                                     {{-- {!! str_replace(['{', '}'], '', $text) !!} --}}
                                 </div>
                                 <footer class="entry-footer d-flex flex-wrap align-items-center mt-4">
-                                    @if(Str::length($text)>0 && Str::length($title)>0)
-                                    <a href="#" class="button gradient-bg">{{__("words.read_more")}}</a>
+                                    @if (Str::length($text) > 0 && Str::length($title) > 0)
+                                        <a href="#" class="button gradient-bg">{{ __('words.read_more') }}</a>
                                     @endif
                                 </footer>
                             </div>
