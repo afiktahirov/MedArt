0da99de4b93cc3edc6754efbc0694dfa726fa236
Afik Tahirov <afik.t38@gmail.com>
2023-10-31 22:20:38 +0400

    update dashboard panel and ticket controller

--- resources/views/admin/partials/panel.blade.php
+++ resources/views/admin/partials/panel.blade.php
@@ -1,155 +1,146 @@
-@extends("admin.index")
-@section("content")
-<main>
-            <div class="header">
-                <div class="left">
-                    <h1>Məlumat Paneli</h1>
-                    <ul class="breadcrumb">
-                        <li><a href="#">
-                                Admin
-                            </a></li>
-                        /
-                        <li><a href="#" class="active">Məlumat Paneli</a></li>
-                    </ul>
-                </div>
-                <a href="#" class="report">
-                    <i class='bx bx-cloud-download'></i>
-                    <span>Yüklə CSV</span>
-                </a>
+@extends('admin.index')
+@section('content')
+    <main>
+        <div class="header">
+            <div class="left">
+                <h1>Məlumat Paneli</h1>
+                <ul class="breadcrumb">
+                    <li><a href="#">
+                            Admin
+                        </a></li>
+                    /
+                    <li><a href="#" class="active">Məlumat Paneli</a></li>
+                </ul>
             </div>
+            <a href="#" class="report">
+                <i class='bx bx-cloud-download'></i>
+                <span>Yüklə CSV</span>
+            </a>
+        </div>
 
-            <!-- Insights -->
-            <ul class="insights">
-                <li id="dateInfo">
-                    <i class='bx bx-calendar-check'></i>
-                    <span class="info">
-                        <h3>
+        <!-- Insights -->
+        <ul class="insights">
+            <li id="dateInfo">
+                <i class='bx bx-calendar-check'></i>
+                <span class="info">
+                    <h3>
 
-                        </h3>
-                        <p></p>
-                    </span>
-                </li>
-                <li><i class='bx bx-show-alt'></i>
-                    <span class="info">
-                        <h3>
-                            944
-                        </h3>
-                        <p>Ümumi baxış sayı</p>
-                    </span>
-                </li>
-                <li><i class='bx bx-line-chart'></i>
-                    <span class="info">
-                        <h3>
-                            721
-                        </h3>
-                        <p>Axtarışda görünmə sayı</p>
-                    </span>
-                </li>
-                <li><i class='bx bxs-no-entry'></i>
-                    <span class="info">
-                        <h3>
-                            42
-                        </h3>
-                        <p>Bloklanan istifadəçi sayı</p>
-                    </span>
-                </li>
-            </ul>
-            <!-- End of Insights -->
+                    </h3>
+                    <p></p>
+                </span>
+            </li>
+            <li><i class='bx bx-show-alt'></i>
+                <span class="info">
+                    <h3>
+                        944
+                    </h3>
+                    <p>Ümumi baxış sayı</p>
+                </span>
+            </li>
+            <li><i class='bx bx-line-chart'></i>
+                <span class="info">
+                    <h3>
+                        721
+                    </h3>
+                    <p>Axtarışda görünmə sayı</p>
+                </span>
+            </li>
+            <li><i class='bx bxs-no-entry'></i>
+                <span class="info">
+                    <h3>
+                        42
+                    </h3>
+                    <p>Bloklanan istifadəçi sayı</p>
+                </span>
+            </li>
+        </ul>
+        <!-- End of Insights -->
 
-            <div class="bottom-data">
-                <div class="orders">
-                    <div class="header">
-                        <i class='bx bx-receipt'></i>
-                        <h3>Ən son gələn müraciətlər</h3>
-                        <i class='bx bx-filter'></i>
-                        <i class='bx bx-search'></i>
-                    </div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>User</th>
-                                <th>Order Date</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <img src="images/profile-1.jpg">
-                                    <p>John Doe</p>
-                                </td>
-                                <td>14-08-2023</td>
-                                <td><span class="status completed">Completed</span></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <img src="images/profile-1.jpg">
-                                    <p>John Doe</p>
-                                </td>
-                                <td>14-08-2023</td>
-                                <td><span class="status pending">Pending</span></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <img src="images/profile-1.jpg">
-                                    <p>John Doe</p>
-                                </td>
-                                <td>14-08-2023</td>
-                                <td><span class="status process">Processing</span></td>
-                            </tr>
-                        </tbody>
-                    </table>
+        <div class="bottom-data">
+            <div class="orders">
+                <div class="header">
+                    <i class='bx bx-receipt'></i>
+                    <h3>Ən son gələn müraciətlər</h3>
+                    <i class='bx bx-filter'></i>
+                    <i class='bx bx-search'></i>
                 </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Pasiyent</th>
+                            <th>Göndərilmə tarixi</th>
+                            <th>Müraciət olunan şöbə</th>
+                            <th>Müraciət olunan həkim</th>
+                            <th>Pasiyentin nömrəsi</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($tickets as $ticket)
+                            <tr>
+                                <td>
+                                    {{ $ticket->name_patient }}
+                                </td>
+                                <td>{{ $ticket->date }}</td>
+                                <td>
 
-                <!-- Reminders -->
-                <div class="reminders">
-                    <div class="header">
-                        <i class='bx bx-note'></i>
-                        <h3>Gələn Müraciətlər</h3>
-                        <i class='bx bx-filter'></i>
-                        <i class='bx bx-plus'></i>
-                    </div>
-                    <ul class="task-list">
-                        @foreach ($tickets as $ticket )
+                                    {{-- @dd($departments) --}}
+                                    @foreach ($departments as $department)
+                                        @php
+                                            if (!count($department->languages)) {
+                                                $departmentName = '';
+                                            } else {
+                                                $departmentName = $department->languages[0]->name;
+                                            }
+                                        @endphp
+                                        {{$departmentName}}
+                                    @endforeach
+                                </td>
+                                <td>
+                                    @if (count($doctors))
+                                        @foreach ($doctors as $doctor)
+                                            @if ($doctor->id == $ticket->doctor)
+                                                {{ $doctor->name }}
+                                            @endif
+                                        @endforeach
+                                    @endif
+                                </td>
+                                <td>
 
-                        <li class="completed">
-                            <div class="task-title">
-                                <p>{{$ticket->name_patient}}</p>
-                            </div>
-                            <div class="task-title">
-                                <p>{{$ticket->patient_phone}}</p>
-                            </div>
-                        </li>
-                        {{-- not-completed --}}
+                                    {{ $ticket->patient_phone }}
+                                </td>
+                                <td><span class="status completed">Baxılıb</span></td>
+                            </tr>
                         @endforeach
-                    </ul>
-                </div>
-
-                <!-- End of Reminders-->
-
+                    </tbody>
+                </table>
             </div>
 
-        </main>
-        <script>
-            function updateClock() {
-                var now = new Date();
-                var currentDate = now.getDate();
-                var currentMonth = now.toLocaleString('default', { month: 'long' });
-                var currentYear = now.getFullYear();
-                var currentTime = now.toLocaleTimeString();
-
-                var dateInfo = document.getElementById('dateInfo');
-                dateInfo.querySelector('h3').textContent = currentDate + ' ' + currentMonth + ' ' + currentYear;
-                dateInfo.querySelector('p').textContent = currentTime;
-            }
-
-            updateClock();
-
-            setInterval(updateClock, 1000);
-        </script>
+            <!-- Reminders -->
 
 
+            <!-- End of Reminders-->
 
+        </div>
 
+    </main>
+    <script>
+        function updateClock() {
+            var now = new Date();
+            var currentDate = now.getDate();
+            var currentMonth = now.toLocaleString('default', {
+                month: 'long'
+            });
+            var currentYear = now.getFullYear();
+            var currentTime = now.toLocaleTimeString();
 
+            var dateInfo = document.getElementById('dateInfo');
+            dateInfo.querySelector('h3').textContent = currentDate + ' ' + currentMonth + ' ' + currentYear;
+            dateInfo.querySelector('p').textContent = currentTime;
+        }
+
+        updateClock();
+
+        setInterval(updateClock, 1000);
+    </script>
 @endsection
--- resources/views/home/partials/homepage-boxes.blade.php
+++ resources/views/home/partials/homepage-boxes.blade.php
@@ -3,55 +3,70 @@
         <div class="row">
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="opening-hours">
-                    <h2 class="d-flex align-items-center">{{__("words.Opening_Hours")}}</h2>
+                    <h2 class="d-flex align-items-center">{{ __('words.Opening_Hours') }}</h2>
                     <ul class="p-0 m-0">
-                        <li>{{__("words.monday")}} <span>8.00 - 19.00</span></li>
-                        <li>{{__("words.thursday")}} <span>8.00 - 19.00</span></li>
-                        <li>{{__("words.friday")}} <span>8.00 - 18.30</span></li>
-                        <li>{{__("words.saturday")}} <span>9.30 - 17.00</span></li>
-                        <li>{{__("words.sunday")}} <span>9.30 - 15.00</span></li>
+                        <li>{{ __('words.monday') }} <span>8.00 - 19.00</span></li>
+                        <li>{{ __('words.thursday') }} <span>8.00 - 19.00</span></li>
+                        <li>{{ __('words.friday') }} <span>8.00 - 18.30</span></li>
+                        <li>{{ __('words.saturday') }} <span>9.30 - 17.00</span></li>
+                        <li>{{ __('words.sunday') }} <span>9.30 - 15.00</span></li>
                     </ul>
                 </div>
             </div>
             <div class="col-12 col-md-6 col-lg-3 mt-5 mt-md-0">
                 <div class="emergency-box">
-                    <h2 class="d-flex align-items-center">{{__("words.Emergency")}}</h2>
+                    <h2 class="d-flex align-items-center">{{ __('words.Emergency') }}</h2>
                     <div class="call-btn button gradient-bg">
                         <a class="d-flex justify-content-center align-items-center" href="#"><img
-                                src="{{asset("images/emergency-call.png")}}"> +994773883238</a>
+                                src="{{ asset('images/emergency-call.png') }}"> +994773883238</a>
                     </div>
                     <div class="infoNowDiv d-flex align-items-center" style="width: 100%;height:105px;">
 
-                        <p class="form-control" style="font-size: 16px;">{{__("words.infoNow")}}</p>
+                        <p class="form-control" style="font-size: 16px;">{{ __('words.infoNow') }}</p>
                     </div>
                 </div>
             </div>
             <div class="col-12 col-md-6 col-lg-5 mt-5 mt-lg-0">
                 <div class="appointment-box">
-                    <h2 class="d-flex align-items-center">{{__("words.Make_an_Appointment")}}</h2>
-                    <form class="d-flex flex-wrap justify-content-between" method="POST" action="{{route("ticket.add")}}">
+                    <h2 class="d-flex align-items-center">{{ __('words.Make_an_Appointment') }}</h2>
+                    <form class="d-flex flex-wrap justify-content-between" method="POST"
+                        action="{{ route('ticket.add') }}">
                         @csrf
                         <select class="select-department" name="select-department">
-                            <option value="value1">{{__("words.select_department")}}</option>
+                            <option value="value1">{{ __('words.select_department') }}</option>
                             @foreach ($departments as $department)
-                            @php
-                                if(!count($department->languages)){
-                                    $departmentName = "";
-                                }
-                                else {
-                                    $departmentName = $department->languages[0]->name;
-                                }
-                            @endphp
-                            <option value="{{$department->id}}">{{$departmentName}}</option>
+                                @php
+                                    if (!count($department->languages)) {
+                                        $departmentName = '';
+                                    } else {
+                                        $departmentName = $department->languages[0]->name;
+                                    }
+                                @endphp
+                                <option value="{{ $department->id }}">{{ $departmentName }}</option>
                             @endforeach
                         </select>
                         <select class="select-doctor" name="select-doctor">
-                            <option>{{__("words.select_doctor")}}</option>
+                            <option>{{ __('words.select_doctor') }}</option>
                         </select>
-                        <input type="text"  name="p_name" placeholder="{{__("words.yourname")}}">
-                        <input type="number" name="p_phone" placeholder="{{__("words.phoneNo")}}">
-                        <input class="button gradient-bg" type="submit" value="{{__("words.boom_appoitnmentt")}}">
+                        <input type="text" name="p_name" placeholder="{{ __('words.yourname') }}">
+                        <input type="number" name="p_phone" placeholder="{{ __('words.phoneNo') }}">
+                        <input class="button gradient-bg" type="submit" value="{{ __('words.boom_appoitnmentt') }}">
                     </form>
+                    @if (session('success'))
+                        <div id="myAlert" class="alert alert-success d-flex justify-content-between" role="alert">
+                            <a href="#" class="alert-link">
+                                <li>{{ session('success') }}</li>
+                            </a>
+                            {{-- <a class="close_alert" style="font-size: 20px; cursor: pointer;">X</a --}}
+                        </div>
+                    @endif
+                    @if (count($errors))
+                    <div id="myAlert" class="alert alert-warning d-flex justify-content-between" role="alert">
+                        <a href="#" class="alert-link">
+                           <li>Müraciət formu düzgün doldurulmayıb</li>
+                        </a>
+                    </div>
+                  @endif
                 </div>
             </div>
         </div>
@@ -59,26 +74,25 @@
 </div>
 
 <script>
-    document.addEventListener("DOMContentLoaded",function(){
-    //Department selected
-    document.querySelector(".select-department").addEventListener('change',function(select){
-        let departmentId = this.value;
-        let doctorSelect = document.querySelector(".select-doctor");
+    document.addEventListener("DOMContentLoaded", function() {
+        //Department selected
+        document.querySelector(".select-department").addEventListener('change', function(select) {
+            let departmentId = this.value;
+            let doctorSelect = document.querySelector(".select-doctor");
 
-        doctorSelect.innerHTML = '';
+            doctorSelect.innerHTML = '';
 
-         fetch('api/getdoctors/'+ departmentId)
-         .then(response=>response.json())
-         .then(doctors=>{
-            doctors.forEach(doctor =>{
-               let option = document.createElement("option");
-               option.value = doctor.id;
-               option.text = doctor.name+" - "+doctor.position;
-               doctorSelect.append(option);
-            });
-         })
-         .catch(error=>console.error("Xeta",error));
+            fetch('api/getdoctors/' + departmentId)
+                .then(response => response.json())
+                .then(doctors => {
+                    doctors.forEach(doctor => {
+                        let option = document.createElement("option");
+                        option.value = doctor.id;
+                        option.text = doctor.name + " - " + doctor.position;
+                        doctorSelect.append(option);
+                    });
+                })
+                .catch(error => console.error("Xeta", error));
+        })
     })
-})
-
 </script>
